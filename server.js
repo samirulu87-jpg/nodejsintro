42ae786f52b53e7d61bb846c836e0628162a4789
Samantha Anderson <samirulu87@gmail.com>
2026-02-02 09:54:20 -0700

    Added base code

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-jjfjjfjjffjgnvn
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.length("/", (req, res) => {
+    res.send("Hello world!Q");
+});
+
+app.listen(3000, () => {
+console.log(`Test server running on port: ${PORT}`);
+});
